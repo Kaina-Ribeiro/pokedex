aa567400282e17b5025f49c0278818db30370f80
Gabriel.Ribeiro <gabrielkaina99@gmail.com>
2022-02-18 02:34:12 -0300

    pokemon list

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
     PokeSearchComponent,
-    PokeListComponent
+    PokeListComponent,
+
   ],
   exports: [
     PokeHeaderComponent,
@@ -18,7 +20,8 @@ import { PokeListComponent } from './poke-list/poke-list.component';
     PokeListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
